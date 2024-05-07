e5eea63550383fa9195b157b49452915b07c718c
Caitlin Schaeffer <caitlinschaef@gmail.com>
2024-05-07 13:01:15 -0400

    about to add masonry

--- src/Pages/Portfolio.jsx
+++ src/Pages/Portfolio.jsx
@@ -3,6 +3,29 @@ import ThemeProvider from 'react-bootstrap/ThemeProvider'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import { FaPython } from "react-icons/fa";
+import { FaHtml5 } from "react-icons/fa";
+import { FaCss3Alt } from "react-icons/fa";
+import { TbBrandJavascript } from "react-icons/tb";
+import { FaBootstrap } from "react-icons/fa";
+import { FaReact } from "react-icons/fa";
+
+//portfolio image imports
+
+const ChildA = () => {
+    return (
+        <a
+        className="masonryImage"
+        href="https://github.com/CaitlinSchaef"
+        >
+            <img
+                alt="Headshot"
+                src={Headshot}
+                className="text-center"
+            />
+        </a>
+    )
+    }
 
 
 const Body = () => {
@@ -17,7 +40,41 @@ const Body = () => {
                 had to make xs=12 so that it was full width on the smallest screen */}
                 <Col xs={12} md={8} className="d-flex flex-column justify-content-between mb-3 text-center aboutBody">
                     <h1><strong>Portfolio:</strong></h1>
-                        <p className="overflow-scroll" style={{height: "65vh"}}></p>
+                    <span>
+                    <a
+                    className="iconReact"
+                    >
+                        <FaHtml5 />
+                    </a>
+                    <a
+                    className="iconReact"
+                    >
+                        <FaCss3Alt />
+                    </a>
+                    <a
+                    className="iconReact"
+                    >
+                        <FaBootstrap />
+                    </a>
+                    <a
+                    className="iconReact"
+                    >
+                        <TbBrandJavascript />
+                    </a>
+                    <a
+                    className="iconReact"
+                    >
+                        <FaReact />
+                    </a>
+                    <a
+                    className="iconReact"
+                    >
+                        <FaPython />
+                    </a>
+                    </span>
+                        <p className="overflow-scroll" style={{height: "65vh"}}>
+                            This is my portfolio
+                        </p>
                 </Col>
             </Row>
         </Container>
