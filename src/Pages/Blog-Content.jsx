26f2fceb664ce4e455af61c5e13532804610761e
Caitlin Schaeffer <caitlinschaef@gmail.com>
2024-05-13 16:57:55 -0400

    display is up but buttons not working

---
+++ src/Pages/Blog-Content.jsx
@@ -0,0 +1,146 @@
+import Fuji from '../assets/fuji-uncut.jpeg'
+import Uni from '../assets/university.jpeg'
+import Berlin from '../assets/berlinEdit.jpeg'
+
+
+const Content_Array = 
+[
+    {
+        "id": "week 11",
+        "title": "Sprint Seven: SQL Databases", 
+        "date": "May 13, 2024",
+        "content": `in progress`,
+        "images": ''
+    },
+    {
+        "id": "week 10",
+        "title": "Sprint Six: Halfway - Python Card Game", 
+        "date": "May 6, 2024",
+        "content": `in progress`,
+        "images": ''
+    },
+    {
+        "id": "week 9",
+        "title": "Sprint Five: CLI", 
+        "date": "April 29, 2024",
+        "content": `in progress`,
+        "images": ''
+    },
+    {
+        "id": "week8",
+        "title": "Sprint Four",
+        "date": "April 22, 2024",
+        "content": " in progress ",
+        "images": "images"
+      },
+      {
+        "id": "week7",
+        "title": "Sprint Three",
+        "date": "April 15, 2024",
+        "content": " in progress ",
+        "images": "images"
+      },
+      {
+        "id": "week6",
+        "title": "Sprint Two",
+        "date": "April 8, 2024",
+        "content": `  <h3>What did you learn about that helped you understand the development process?</h3>
+        <p>I think learning about APIs was just another level of abstraction to learn. Not only are we learning how to code, but we are learning <em>how</em> the things we will develop work and how they fit into the overall flow of the web.</p>
+        
+        <h3>What does it mean to develop good code?</h3>
+        <p>I think good code has two really big indicators: is it human-readable and is it flexible? If code can be easily understood by other developers, it is easier to work with. If it is flexible, it is easy to add to or take away from, and would in theory develop more easily.</p>
+        
+        <h3>What does it mean to be a good developer?</h3>
+        <p>I think good developers are agile, curious, and humble.</p>
+        
+        <h3>What is one thing that programmers hate doing?</h3>
+        <p>I'm not sure yet! I will come back to this question near the end of the program.</p>
+      `,
+        "images": ""
+      },
+      {
+        "id": "week5",
+        "title": "Sprint One",
+        "date": "April 1, 2024",
+        "content": `  <h3>What is the number one thing that held you back this week? Why?</h3>
+        <p>I think that the only thing holding me back right now is my limited scope of knowledge. I started coding 5 weeks ago, at the beginning of March 2024. Being in the 5th week of the program encompasses the full range of my knowledge.</p>
+        
+        <h3>What is one thing you would like to learn more about regarding JavaScript (JSON, Data Structures), Bootstrap, or Atomic Design?</h3>
+        <p>I think that I would like to learn more functions of JavaScript, and continue to refine my understanding of functions, variables, and other components.</p>
+        
+        <h3>What is your “Why”?</h3>
+        <p>My 'why' is developing a career in a field I'm interested in, that will allow me to live the kind of life I find fulfilling.</p>
+      `,
+        "images": ""
+      },
+      {
+        "id": "week4",
+        "title": "Fourth Week",
+        "date": "March 25, 2024",
+        "content": `  <h3>What was helpful during Onboarding working remotely?</h3>
+        <p>I think the program being more flexible while we're going through a lot of information is helpful, so that we can read and participate as we are able.</p>
+        
+        <h3>What hindered your progress during Onboarding?</h3>
+        <p>Working my job as a teacher while Onboarding was going on was most definitely challenging thing.</p>
+        
+        <h3>How did you overcome challenges during Onboarding?</h3>
+        <p>Trying to be cognizant of timing and timelines, doing things again and again, and eventually learning when to ask for help.</p>
+      `,
+        "images": ""
+      },
+      {
+        "id": "week3",
+        "title": "Third Week",
+        "date": "March 18, 2024",
+        "content": `  <h3>What are your thoughts about the roles HTML, CSS, and JavaScript play in the process of rendering content and providing a user experience?</h3>
+        <p>I think that I'm starting to see how everything comes together, which makes it less scary. Coming in with no real coding experience means that there was just oceans of information. There're still oceans but I think I could name at least one now.</p>
+        
+        <h3>What are some objects that your blog incorporates?</h3>
+        <p>My blog incorporates pictures (which I think would be considered objects?). This question honestly confuses me a little, as our blogs so far don't have any javascript aspects.</p>
+      `,
+        "images": ""
+      },
+      {
+        "id": "week2",
+        "title": "Second Week",
+        "date": "March 11, 2024",
+        "content": `  <h3>What is something you wish you could do but currently do not have the coding skills to accomplish?</h3>
+        <p>I would really like to be better able to organize a website. Making it have a great user experience and good flow would be fantastic. I am going to practice by restructuring this blog!</p>
+        
+        <h3>What are you struggling with?</h3>
+        <p>Right now I'm struggling with finding the balance amongst things, there is just a lot happening all at once with teaching, starting this program, and a general busy season of life. I keep thinking about how nice it will be in 6 months, when I will hopefully have a job and focus on that exclusively.</p>
+        
+        <h3>How do you solve a problem?</h3>
+        <p>I tend to really hammer at something until I understand what's happening, almost too much.</p>
+        
+        <h3>What methods do you use to help yourself get unstuck?</h3>
+        <p>I think the best method for getting unstuck is to try and broaden the scope of what you're looking at. Often, problems are symptomatic of a breakdown somewhere else, so it is good to try and think of the bigger picture.</p>
+      `,
+        "images": ""
+      },
+        {
+          "id": "week1",
+          "title": "First Week",
+          "date": "March 4, 2024",
+          "content": `  <h3>Was your first week what you expected? Why? Why not?</h3>
+          <p>The first week of bootcamp exceeded my expectations in both workload and excitement. We dove right in to an ocean of material!</p>
+          
+          <h3>What are you excited or eager to learn more about?</h3>
+          <p>I am excited to learn more about back end development, as it is honestly the part of web development that is a bit scary to me.</p>
+          
+          <h3>What is something about you that can only be learned by reading this blog?</h3>
+          <p>I would say that I am much nerdier than I may appear, and it often surprises people. I'm a big fan of things like:</p>
+          <ul>
+          <li>Video games (Play Station, Nintendo Switch, XBox, etc.)</li>
+          <li>Anime (I'm up to date on One Piece, and I like shonen shows, but also love a nice slice-of-life show, my favorite at the moment is The Apothecary Diaries {think lady Sherlock Holmes set in something like a pre-feudal Japan}).</li>
+          <li>Novels (I recommend <em>The Poppy Wars</em>, the <em>Red Rising</em> series, and also classical works like The Odyssey).</li>
+          <li>DnD (I've even been a DM, ✨dungeon master✨, for a few campaigns).</li>
+          </ul>
+          <p>Most of all, I love traveling and experiencing new cultures (and learning their languages).</p>
+          <p>Please enjoy a few pictures from the two countries I visited last year in 2023, Scotland and Japan (including one picture of Mount Fuji on a clear day!):</p>
+        `,
+          "images": ""
+        }
+]
+
+export default Content_Array
--- src/Pages/Blog.jsx
+++ src/Pages/Blog.jsx
@@ -1,15 +1,44 @@
 
 import { Link } from "react-router-dom"
+import { useState } from "react"
 import ThemeProvider from 'react-bootstrap/ThemeProvider'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
-import Fuji from '../assets/fuji-uncut.jpeg'
-import Uni from '../assets/university.jpeg'
-import Berlin from '../assets/berlinEdit.jpeg'
-import { GiFuji } from "react-icons/gi"
+import Content_Array from "./Blog-Content"
+import { Button } from "react-bootstrap"
+
+
+
+const BlogDisplay = ({ content }) => {
+  const mostRecent = content[0]
+  return (
+    <div>
+      <h2 className="CurrentTitle"> {mostRecent.title} </h2>
+      <h5 className="CurrentDate">{mostRecent.date} </h5>
+      <p className="CurrentContent overflow-scroll" style={{height: "65vh"}}> {mostRecent.content} </p>
+    </div>
+  )
+}
+
+// back Button
+// index + 1
+// next Button
+// index -1
+// it feels backwards, but since the most recent index is 0, to go back in time we need to add and to bring it back to the
+// present we need to subtract
 
 const Body = () => {
+  const [content, setContent] = useState(Content_Array);
+  const [contentIndex, setContentIndex] = useState(0);
+  //do we need to make a useEffect for rendering?
+
+  const handleNext = () => {
+    setContentIndex((prevIndex) => (prevIndex < content.length - 1 ? prevIndex + 1 : prevIndex) )
+  }
+  const handleBack = () => {
+    setContentIndex((prevIndex) => (prevIndex > 0 ? prevIndex - 1 : prevIndex))
+  }
   return (
     <ThemeProvider
     breakpoints={['xxxl', 'xxl', 'xl', 'lg', 'md', 'sm', 'xs']}
@@ -22,7 +51,11 @@ const Body = () => {
         had to make xs=12 so that it was full width on the smallest screen */}
         <Col xs={12} md={8} className="d-flex flex-column justify-content-between mb-3 text-center aboutBody">
               <h1><strong>Blog:</strong></h1>
-                
+                <div>
+                  <BlogDisplay content={content} />
+                  <Button className="myButton" onClick={handleBack} disable={contentIndex === 0}>Previous</Button>
+                  <Button className="myButton" onClick={handleNext} disable={contentIndex === content.length - 1}>Next</Button>
+                </div>
         </Col>
       </Row>
     </Container>
